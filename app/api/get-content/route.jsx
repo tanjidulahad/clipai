324867ece1d25b44c87983eed24d4153f6db2e34
tanjidulahad <ahad@sigaramtech.com>
2024-10-30 20:34:45 +0600

    little fix

--- app/api/get-content/route.jsx
+++ app/api/get-content/route.jsx
@@ -1,8 +1,5 @@
 import UserContent from "@/Model/usersContent"
-import connectDB from "@/utils/connectDB"
 import { NextResponse } from "next/server"
-
-await connectDB()
 
 export async function GET(req){
 
